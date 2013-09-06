045c74ff84908801d338e62144164d7ac887e295
davemacp <dave@davemacp.com>
2013-09-06 14:01:13 -0300

    Update Test1.php

--- Test1.php
+++ Test1.php
@@ -6,8 +6,7 @@
 ?>
 <Response>
     <Say>Hi, you've reached the offices of LiveCA, Chartered Accountant.</Say>
-    <Play>http://demo.twilio.com/hellomonkey/monkey.mp3</Play>
-    <Gather numDigits="1" action="hello-monkey-handle-key.php" method="POST">
+    <Gather numDigits="1" action="https://github.com/davemacp/dmacptwilio/blob/master/Test2.php" method="POST">
         <Say>To speak to one of us, press 1.  To leave a message, press 2. Press any other key to start 	over.</Say>
     </Gather>
 </Response>
